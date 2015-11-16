39255e43ecdae6085067c3d7a29f8981c6cf29d6
fotoply <norberg.niels@gmail.com>
2015-11-16 17:28:10 +0100

    Working glow on the buttons. Also added some temporary scenes

--- SemesterProjektHighFidelity/src/project/view/manager/LeftMenuController.java
+++ SemesterProjektHighFidelity/src/project/view/manager/LeftMenuController.java
@@ -3,9 +3,13 @@ package project.view.manager;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
+import javafx.scene.paint.Color;
+
+import java.util.ArrayList;
 
 /**
  * Created 11/13/15
@@ -16,6 +20,9 @@ public class LeftMenuController {
     ImageView allPlacesImage = new ImageView(new Image(getClass().getResourceAsStream("/project/res/button_all_places_icon.png")));
     ImageView notificationImage = new ImageView(new Image(getClass().getResourceAsStream("/project/res/button_notification_icon.png")));
     ImageView messageImage = new ImageView(new Image(getClass().getResourceAsStream("/project/res/button_message_icon.png")));
+
+    ArrayList<Button> buttons = new ArrayList<>();
+
     @FXML
     private Button allPlacesButton;
     @FXML
@@ -25,6 +32,10 @@ public class LeftMenuController {
 
     @FXML
     private void initialize() {
+        buttons.add(allPlacesButton);
+        buttons.add(notificationsButton);
+        buttons.add(messagesButton);
+
         allPlacesButton.setGraphic(allPlacesImage);
         notificationsButton.setGraphic(notificationImage);
         messagesButton.setGraphic(messageImage);
@@ -46,6 +57,38 @@ public class LeftMenuController {
         messagesButton.setScaleX(0.5);
         messagesButton.setScaleY(0.5);
 
+    }
+
+    private void selectButton(Button b) {
+        for (Button button : buttons) {
+            if (button != b && button.getEffect() != null) {
+                button.setEffect(null);
+            }
+        }
+
+        DropShadow borderGlow = new DropShadow();
+        borderGlow.setOffsetY(0f);
+        borderGlow.setOffsetX(0f);
+        borderGlow.setColor(Color.YELLOW);
+        borderGlow.setWidth(80);
+        borderGlow.setHeight(80);
+
+        b.setEffect(borderGlow);
+    }
+
+    @FXML
+    private void allPlacesClicked() {
+        selectButton(allPlacesButton);
+    }
+
+    @FXML
+    private void notificationsClicked() {
+        selectButton(notificationsButton);
+    }
+
+    @FXML
+    private void messagesClicked() {
+        selectButton(messagesButton);
     }
 
 }
