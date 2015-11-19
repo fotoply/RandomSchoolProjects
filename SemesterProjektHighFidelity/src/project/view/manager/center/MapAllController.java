b608d57fe712be2acfcb7f27346bf8cc7053e34c
fotoply <norberg.niels@gmail.com>
2015-11-19 22:43:16 +0100

    Very first look at map. Kinda works but also kinda slow. Not even used yet.

--- SemesterProjektHighFidelity/src/project/view/manager/center/MapAllController.java
+++ SemesterProjektHighFidelity/src/project/view/manager/center/MapAllController.java
@@ -1,8 +1,15 @@
 package project.view.manager.center;
 
+import com.lynden.gmapsfx.GoogleMapView;
+import com.lynden.gmapsfx.MapComponentInitializedListener;
+import com.lynden.gmapsfx.javascript.object.GoogleMap;
+import com.lynden.gmapsfx.javascript.object.LatLong;
+import com.lynden.gmapsfx.javascript.object.MapOptions;
+import com.lynden.gmapsfx.javascript.object.MapTypeIdEnum;
 import javafx.animation.Transition;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.layout.BorderPane;
 import project.view.AnimationHelper;
 import project.view.manager.OpenCloseAnimated;
 
@@ -11,13 +18,27 @@ import project.view.manager.OpenCloseAnimated;
  *
  * @author Niels Norberg
  */
-public class MapAllController implements OpenCloseAnimated {
+public class MapAllController implements OpenCloseAnimated, MapComponentInitializedListener {
+
+    GoogleMapView mapView;
+    GoogleMap map;
     @FXML
     Node node;
+
+    @Override
+    public void mapInitialized() {
+        MapOptions mapOptions = new MapOptions();
+        mapOptions.center(new LatLong(56.1351841, 8.1906375)).zoom(4).overviewMapControl(false).panControl(false).rotateControl(false).scaleControl(false).streetViewControl(false).zoomControl(false).mapType(MapTypeIdEnum.ROADMAP);
+
+        map = mapView.createMap(mapOptions);
+    }
 
     @FXML
     private void initialize() {
         AnimationHelper.initializeSlideFadeFromRight(node);
+        mapView = new GoogleMapView();
+        mapView.addMapInializedListener(this);
+        ((BorderPane) node).setCenter(mapView);
     }
 
     @Override
